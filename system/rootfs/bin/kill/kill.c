045807845bc1f57935b6fa479e02a04b1d65a7b1
misazhu <misa.zhu@gmail.com>
2019-11-24 20:44:22 +0800

    kill app

---
+++ system/rootfs/bin/kill/kill.c
@@ -0,0 +1,15 @@
+#include <cmain.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <syscall.h>
+
+int main(int argc, char* argv[]) {
+	if(argc < 2) {
+		printf("Usage: kill <pid>.\n");
+		return -1;
+	}
+
+	int pid = atoi(argv[1]);
+	return syscall1(SYS_KILL, pid);
+}
+
