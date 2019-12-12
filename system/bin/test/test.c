3f5a487b8ecc466c96b8427b11e36c80bdbca75e
May the source be with you <misa@Misas-iMac.local>
2019-12-12 15:33:23 +0800

    libs link

--- system/bin/test/test.c
+++ system/bin/test/test.c
@@ -2,27 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <mario/mario_vm.h>
-
-
-const char* js = " \
-	var s = \"Misa\"; \
-  function hello() { \
-    debug(\"Hello JS world!\" + s); \
-  } \
-  hello();";
 
 int main(int argc, char** argv) {
 	(void)argc;
 	(void)argv;
-
-  vm_t* vm = vm_new(compile);
-
-  vm_init(vm, NULL, NULL); //initialize the vm enviroment.
-
-  vm_load_run(vm, js); // load JS script (and compile to bytecode) and run
-
-  vm_close(vm); //release
   return 0;
 }
 
