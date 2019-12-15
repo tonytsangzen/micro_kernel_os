4fb8d3517055a15320bd7f25a58f6ead2ecf05a2
Misa.Z <misa@macbook-pro17.local>
2019-12-16 01:39:38 +0800

    arch arm-raspi2

---
+++ kernel/arch/arm/raspi2/hw_info.c
@@ -0,0 +1,10 @@
+#include <kernel/hw_info.h>
+#include <mm/mmu.h>
+
+hw_info_t _hw_info;
+
+void hw_info_init(void) {
+	_hw_info.phy_mem_size = 256*MB;
+	_hw_info.phy_mmio_base = 0x3F000000;
+	_hw_info.mmio_size = 4*MB;
+}
